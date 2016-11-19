d17b315a7ceb02d3980c57cea2fecbac4381797a
Vlad Mocanu <vladmoca90@gmail.com>
2016-11-19 12:37:31 +0000

    Added a JSON object model for a flight

--- Flight Agenda/scripts/flightList.js
+++ Flight Agenda/scripts/flightList.js
@@ -3,3 +3,16 @@
         $(this).parent().remove();
     })
 })
+
+{ 
+    date: "15.11.2016", 
+    flightNumber: "ROT391", 
+    from: "OTP", 
+    to: "LHR",
+    distance: "1309",
+    departure: "12:58",
+    arrival: "14:07",
+    airline: "<img src="#"/>",
+    aircraft: "airbus a318",
+    seat: "20F"
+}
