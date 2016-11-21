ec332538025b41976c3b64aa36960487137922f4
Vlad Mocanu <vladmoca90@gmail.com>
2016-11-21 10:54:36 +0000

    Added the javascript code with the objects in Plans.odt

--- Flight Agenda/scripts/flightList.js
+++ Flight Agenda/scripts/flightList.js
@@ -3,22 +3,3 @@
         $(this).parent().remove();
     })
 })
-
-{
-    date: "15.11.2016",
-    flightNumber: "ROT391",
-    from: "OTP",
-    to: "LHR",
-    distance: "1309",
-    departure: "12:58",
-    arrival: "14:07",
-    airline: {
-        name: "TAROM",
-        logo: "http://blabla.com/avianga.jpg"
-    },
-    aircraft: {
-            name: "Airbus",
-            model: "A318"
-    },
-    seat: "20F"
-}
