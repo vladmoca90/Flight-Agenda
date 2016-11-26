73d207f474be7f3e76675a2db6f37e955ea31dba
Vlad Mocanu <vladmoca90@gmail.com>
2016-11-26 14:40:47 +0000

    Copy and pasted the js code

--- Flight Agenda/scripts/addAFlightForm.js
+++ Flight Agenda/scripts/addAFlightForm.js
@@ -1,1 +1,38 @@
-﻿
+﻿// only for demo purposes
+$.validator.setDefaults({
+
+    submitHandler: function () {
+        alert("submitted!");
+    }
+
+});
+
+$().ready(function () {
+    // validate the form when it is submitted
+    var validator = $("#form1").validate({
+        errorPlacement: function (error, element) {
+            // Append error within linked label
+            $(element)
+				.closest("form")
+					.find("label[for='" + element.attr("id") + "']")
+						.append(error);
+        },
+        errorElement: "span",
+        messages: {
+            user: {
+                required: " (required)",
+                minlength: " (must be at least 3 characters)"
+            },
+            password: {
+                required: " (required)",
+                minlength: " (must be between 5 and 12 characters)",
+                maxlength: " (must be between 5 and 12 characters)"
+            }
+        }
+    });
+
+    $(".cancel").click(function () {
+        validator.resetForm();
+    });
+
+});
