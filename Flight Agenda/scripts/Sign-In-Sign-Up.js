f1acaf150de254fdbe3f85fd80f317c36fc5cb3d
Vlad Mocanu <vladmoca90@gmail.com>
2016-11-27 23:29:28 +0000

    Removed the codes for the #sign-up-section and #sign-in-section in the Sign-In-Sign-Up.js file

--- Flight Agenda/scripts/Sign-In-Sign-Up.js
+++ Flight Agenda/scripts/Sign-In-Sign-Up.js
@@ -1,1 +1,20 @@
-﻿
+﻿$(document).ready(function () {
+
+    $("#sign-in-section").hide();
+
+    $(".sign-in-button").click(function () {
+        $("#sign-in-section").show();
+    });
+
+});
+
+$(document).ready(function () {
+
+    $("#sign-up-section").hide();
+
+    $("#signUpLink").click(function () {
+        $("#sign-up-section").show();
+        $("#sign-in-section").hide();
+    });
+
+});
--- Flight Agenda/scripts/addAFlightForm.js
+++ Flight Agenda/scripts/addAFlightForm.js
@@ -4,27 +4,6 @@
     });
 }); */
 
-$(document).ready(function () {
-
-    $("#sign-in-section").hide();
-
-    $(".sign-in-button").click(function () {
-        $("#sign-in-section").show();
-    });
-
-});
-
-$(document).ready(function () {
-
-    $("#sign-up-section").hide();
-
-    $("#signUpLink").click(function () {
-        $("#sign-up-section").show();
-        $("#sign-in-section").hide();
-    });
-
-});
-
 // Wait for the DOM to be ready
 $(function () {
     // Initialize form validation on the registration form.
