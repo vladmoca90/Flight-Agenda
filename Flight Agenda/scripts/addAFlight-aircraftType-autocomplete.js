c1514c987f6174821626970307cb3bcf1ad30e95
Vlad Mocanu <vladmoca90@gmail.com>
2016-12-01 23:14:37 +0000

    Replaced availableTags with input

--- Flight Agenda/scripts/addAFlight-aircraftType-autocomplete.js
+++ Flight Agenda/scripts/addAFlight-aircraftType-autocomplete.js
@@ -30,7 +30,7 @@
               event.preventDefault();
           }
       }).autocomplete({ minLength: 0, source: function (request, response) {
-              response($.ui.autocomplete.filter(availableTags, extractLast(request.term)));
+              response($.ui.autocomplete.filter(input, extractLast(request.term)));
           },
           focus: function () {
               // prevent value inserted on focus
