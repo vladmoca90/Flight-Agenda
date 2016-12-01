28e0eb58a5d0ad63253884a30e498a3ad3b0f9e3
Vlad Mocanu <vladmoca90@gmail.com>
2016-12-01 13:11:55 +0000

    Created the same autocomplete code for the arrival codes

---
+++ Flight Agenda/scripts/addAFlight-arrival-autocomplete.js
@@ -0,0 +1,64 @@
+﻿$(function () {
+    var arrivalCode = [
+      "CDG",
+      "LHR",
+      "OTP",
+      "FCO",
+      "JFK",
+      "LAX",
+      "TXL",
+      "IST",
+      "HKG",
+      "DEL",
+      "AMS",
+      "MUC",
+      "BCN",
+      "ORD",
+      "BOG",
+      "EZE",
+      "SYD",
+      "PEK",
+      "DME",
+      "SCL",
+      "JNB",
+      "LOS"
+    ];
+    function split(val) {
+        return val.split(/,\s*/);
+    }
+    function extractLast(term) {
+        return split(term).pop();
+    }
+
+    $("#arrival")
+      // don't navigate away from the field on tab when selecting an item
+      .on("keydown", function (event) {
+          if (event.keyCode === $.ui.keyCode.TAB &&
+              $(this).autocomplete("instance").menu.active) {
+              event.preventDefault();
+          }
+      })
+      .autocomplete({
+          minLength: 0,
+          source: function (request, response) {
+              // delegate back to autocomplete, but extract the last term
+              response($.ui.autocomplete.filter(
+                availableTags, extractLast(request.term)));
+          },
+          focus: function () {
+              // prevent value inserted on focus
+              return false;
+          },
+          select: function (event, ui) {
+              var terms = split(this.value);
+              // remove the current input
+              terms.pop();
+              // add the selected item
+              terms.push(ui.item.value);
+              // add placeholder to get the comma-and-space at the end
+              terms.push("");
+              this.value = terms.join(", ");
+              return false;
+          }
+      });
+});
